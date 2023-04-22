a75c4eadbb6adc75ca9f3d83e4749ca5c7c75314
MahmoudEasa <mu01011422865@gmail.com>
2023-04-22 11:03:55 -0700

    Add main.h

---
+++ main.h
@@ -0,0 +1,33 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg>
+#include <unistd>
+
+/**
+ * struct conversion - handle conversion specifiers
+ * @format: pointer to character
+ * @f: pointer to function
+ *
+ * Description: produce output according to a format
+ */
+
+typedef struct conversion
+{
+	char *format;
+	void (*f)(va_list);
+} Conversion;
+
+int _printf(const char *format, ...);
+int handle_c(va_list val);
+int handle_s(va_list val);
+int handle_d(va_list val);
+int handle_i(va_list val);
+int handle_b(va_list val);
+int handle_u(va_list val);
+int handle_o(va_list val);
+int handle_x(va_list val);
+int handle_X(va_list val);
+int handle_s(va_list val);
+int handle_p(va_list val);
+
+#endif /* #ifndef MAIN_H */
