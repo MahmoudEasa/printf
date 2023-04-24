585c819d4f625d3b5599fe51d0de7c63fad8c698
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-23 20:54:13 -0700

    File handle_u.c

---
+++ handle_u.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * handle_u - print the unsigned integer value
+ * @val: va_list
+ * Return: return length
+*/
+
+int handle_u(va_list val)
+{
+	int n, len = 0;
+	char str[20];
+
+	n = va_arg(val, int);
+	if (n >= 0)
+	{
+		sprintf(str, "%d", n);
+		len = strlen(str);
+		write(1, str, len);
+		return (len);
+	}
+	else
+		return (-1);
+}
