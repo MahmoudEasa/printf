95676594423b0de9304fe590a08e5c1296718a47
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-04-25 10:56:43 -0700

    File

--- printf.c
+++ printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
-#define CON_LEN 12
+#define CON_LEN 13
 
 /**
  * _printf - produces output according to a format
@@ -27,6 +27,7 @@ int _printf(const char *format, ...)
 		{"S", handle_S},
 		{"p", handle_p},
 		{"r", handle_r},
+		{"R", handle_R},
 	};
 
 	va_start(arg, format);
